c387aeb2b295e7e61784b072a4e0f5b64f1b9ab5
eshasharma <eshasharma@users.noreply.github.com>
2017-11-30 19:33:33 -0500

    Add files via upload

---
+++ Test Cases/test_case_1.py
@@ -0,0 +1,44 @@
+"""""
+Test Case 1 for TOP 
+Point to Point - calculated the shortest distance between two points in a directed graph 
+Test Case picked from 
+source: https://www.python.org/doc/essays/graphs/
+author: Guido van Rossum
+Assumption : No Negative edges in Graph
+
+Directed Acyclic Graph
+"""""
+
+
+def find_shortest_path(graph, start, end, path=[]):
+    """
+    re='https://www.python.org/doc/essays/graphs/'
+    __author__='Guido van Rossum'
+    """
+    path = path + [start]
+    if start == end:
+        return path
+    if not graph.has_key(start):
+        return None
+    shortest = None
+    for node in graph[start]:
+        if node not in path:
+            newpath = find_shortest_path(graph, node, end, path)
+            if newpath:
+                if not shortest or len(newpath) < len(shortest):
+                    shortest = newpath
+    return shortest
+
+if __name__=='__main__':
+    # Defining the Graph
+    graph = {
+        'a': {'b', 'c'},
+        'b': {'d'},
+        'c': {'e'},
+        'd': {'e', 'c','f'}
+
+    }
+    print(find_shortest_path(graph,'a','d'))
+
+    print(len(find_shortest_path(graph,'a','d')))
+
