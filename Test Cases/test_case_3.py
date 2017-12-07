788580673926d14ca45c783c1c9c710f00d02953
Brian Clee <bpclee2011@gmail.com>
2017-12-07 18:16:36 -0500

    added timing for average of 200 runs

--- Test Cases/test_case_3.py
+++ Test Cases/test_case_3.py
@@ -1,56 +1,47 @@
 """""
-Test Case 3 for TOP 
+Test Case 3 for TOP
 K Means
 Clustering query points into K groups
-Test Case picked from 
+Test Case picked from
 source: https://gist.github.com/iandanforth/5862470
 http://pandoricweb.tumblr.com/post/8646701677/python-implementation-of-the-k-means-clustering
 """""
 import math
 import random
-plotly = False
-try:
-    import plotly
-    from plotly.graph_objs import Scatter, Scatter3d, Layout
-except ImportError:
-    print "INFO: Plotly is not installed, plots will not be generated."
+import time
 
 def main():
+    timeList = []
+    for index in range(1,200):
+        startTime = time.time()
+        num_points = 200
+        dimensions = 2
 
-    # How many points are in our dataset?
-    num_points = 20
+        lower = 0
+        upper = 200
 
-    # For each of those points how many dimensions do they have?
-    # Note: Plotting will only work in two or three dimensions
-    dimensions = 2
+        num_clusters = 5
+        cutoff = 0.2
 
-    # Bounds for the values of those points in each dimension
-    lower = 0
-    upper = 200
+        # Generate some points to cluster
+        points = [
+            makeRandomPoint(dimensions, lower, upper) for i in xrange(num_points)
+        ]
+        clusters = kmeans(points, num_clusters, cutoff)
 
-    # The K in k-means. How many clusters do we assume exist?
-    num_clusters = 3
+        # Print our clusters
+        #for i, c in enumerate(clusters):
+        #    for p in c.points:
+        #        print " Cluster: ", i, "\t Point :", p
 
-    # When do we say the optimization has 'converged' and stop updating clusters
-    cutoff = 0.2
+        endTime = time.time()
+        timeList.append(endTime - startTime)
 
-    # Generate some points to cluster
-    points = [
-        makeRandomPoint(dimensions, lower, upper) for i in xrange(num_points)
-    ]
-
-    # Cluster those data!
-    clusters = kmeans(points, num_clusters, cutoff)
-
-    # Print our clusters
-    for i, c in enumerate(clusters):
-        for p in c.points:
-            print " Cluster: ", i, "\t Point :", p
-
-    # Display clusters using plotly for 2d data
-    if dimensions in [2, 3] and plotly:
-        print "Plotting points, launching browser ..."
-        plotClusters(clusters, dimensions)
+    totalTime = 0
+    for times in timeList:
+        totalTime = totalTime + times
+    averageTime = totalTime / 200
+    print("average completion time: " + str(averageTime))
 
 class Point(object):
     '''
@@ -60,7 +51,6 @@ class Point(object):
         '''
         coords - A list of values, one per dimension
         '''
-
         self.coords = coords
         self.n = len(coords)
 
@@ -76,14 +66,10 @@ class Cluster(object):
         '''
         points - A list of point objects
         '''
-
         if len(points) == 0:
             raise Exception("ERROR: empty cluster")
 
-        # The points that belong to this cluster
         self.points = points
-
-        # The dimensionality of the points in this cluster
         self.n = points[0].n
 
         # Assert that all points are of the same dimensionality
@@ -128,8 +114,6 @@ class Cluster(object):
         return Point(centroid_coords)
 
 def kmeans(points, k, cutoff):
-
-    # Pick out k random points to use as our initial centroids
     initial = random.sample(points, k)
 
     # Create k clusters using those centroids
@@ -153,29 +137,20 @@ def kmeans(points, k, cutoff):
 
             # Set the cluster this point belongs to
             clusterIndex = 0
-
-            # For the remainder of the clusters ...
             for i in range(clusterCount - 1):
                 # calculate the distance of that point to each other cluster's
                 # centroid.
                 distance = getDistance(p, clusters[i+1].centroid)
-                # If it's closer to that cluster's centroid update what we
-                # think the smallest distance is
+
                 if distance < smallest_distance:
                     smallest_distance = distance
                     clusterIndex = i+1
-            # After finding the cluster the smallest distance away
-            # set the point to belong to that cluster
             lists[clusterIndex].append(p)
 
-        # Set our biggest_shift to zero for this iteration
         biggest_shift = 0.0
-
-        # For each cluster ...
         for i in range(clusterCount):
             # Calculate how far the centroid moved in this iteration
             shift = clusters[i].update(lists[i])
-            # Keep track of the largest move from all cluster centroid updates
             biggest_shift = max(biggest_shift, shift)
 
         # If the centroids have stopped moving much, say we're done!
@@ -208,80 +183,6 @@ def makeRandomPoint(n, lower, upper):
     '''
     p = Point([random.uniform(lower, upper) for _ in range(n)])
     return p
-
-def plotClusters(data, dimensions):
-    '''
-    This uses the plotly offline mode to create a local HTML file.
-    This should open your default web browser.
-    '''
-    if dimensions not in [2, 3]:
-        raise Exception("Plots are only available for 2 and 3 dimensional data")
-
-    # Convert data into plotly format.
-    traceList = []
-    for i, c in enumerate(data):
-        # Get a list of x,y coordinates for the points in this cluster.
-        cluster_data = []
-        for point in c.points:
-            cluster_data.append(point.coords)
-
-        trace = {}
-        centroid = {}
-        if dimensions == 2:
-            # Convert our list of x,y's into an x list and a y list.
-            trace['x'], trace['y'] = zip(*cluster_data)
-            trace['mode'] = 'markers'
-            trace['marker'] = {}
-            trace['marker']['symbol'] = i
-            trace['marker']['size'] = 12
-            trace['name'] = "Cluster " + str(i)
-            traceList.append(Scatter(**trace))
-            # Centroid (A trace of length 1)
-            centroid['x'] = [c.centroid.coords[0]]
-            centroid['y'] = [c.centroid.coords[1]]
-            centroid['mode'] = 'markers'
-            centroid['marker'] = {}
-            centroid['marker']['symbol'] = i
-            centroid['marker']['color'] = 'rgb(200,10,10)'
-            centroid['name'] = "Centroid " + str(i)
-            traceList.append(Scatter(**centroid))
-        else:
-            symbols = [
-                "circle",
-                "square",
-                "diamond",
-                "circle-open",
-                "square-open",
-                "diamond-open",
-                "cross", "x"
-            ]
-            symbol_count = len(symbols)
-            if i > symbol_count:
-                print "Warning: Not enough marker symbols to go around"
-            # Convert our list of x,y,z's separate lists.
-            trace['x'], trace['y'], trace['z'] = zip(*cluster_data)
-            trace['mode'] = 'markers'
-            trace['marker'] = {}
-            trace['marker']['symbol'] = symbols[i]
-            trace['marker']['size'] = 12
-            trace['name'] = "Cluster " + str(i)
-            traceList.append(Scatter3d(**trace))
-            # Centroid (A trace of length 1)
-            centroid['x'] = [c.centroid.coords[0]]
-            centroid['y'] = [c.centroid.coords[1]]
-            centroid['z'] = [c.centroid.coords[2]]
-            centroid['mode'] = 'markers'
-            centroid['marker'] = {}
-            centroid['marker']['symbol'] = symbols[i]
-            centroid['marker']['color'] = 'rgb(200,10,10)'
-            centroid['name'] = "Centroid " + str(i)
-            traceList.append(Scatter3d(**centroid))
-
-    title = "K-means clustering with %s clusters" % str(len(data))
-    plotly.offline.plot({
-        "data": traceList,
-        "layout": Layout(title=title)
-    })
 
 if __name__ == "__main__":
     main()
